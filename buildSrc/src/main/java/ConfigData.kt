1cde45ae54919883bbe520c1216a2f2049e6bd61
Nicolas Fillion <nfillion@hubside.com>
2022-10-15 21:51:17 +0200

    switch icons

--- buildSrc/src/main/java/ConfigData.kt
+++ buildSrc/src/main/java/ConfigData.kt
@@ -3,6 +3,6 @@ object ConfigData {
     const val buildToolsVersion = "30.0.3"
     const val minSdkVersion = 21
     const val targetSdkVersion = 31
-    const val versionCode = 1
-    const val versionName = "1.0"
+    const val versionCode = 2
+    const val versionName = "1.1"
 }
